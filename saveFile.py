0ee1472867b30f2d70f5a37cc0139c4f6c5a2828
nopnop2002 <>
2026-06-29 13:13:05 +0900

    Updated paho-mqtt to Version 2

--- saveFile.py
+++ saveFile.py
@@ -5,36 +5,38 @@
 import random
 import socket
 import paho.mqtt.client as mqtt
+import argparse
 
-
-# MQTT Broker
-host = 'esp32-broker.local'
-# MQTT Port
-port = 1883
-# MQTT Subscribe Topic
-topic = '/image/esp32cam'
-# Save File
-saveFile = './output.jpg'
-
-def on_connect(client, userdata, flags, respons_code):
-	print('connect {0} status {1}'.format(host, respons_code))
-	client.subscribe(topic)
+def on_connect(client, userdata, flags, respons_code, properties):
+	print('connect {0} status {1}'.format(args.host, respons_code))
+	client.subscribe(args.topic)
 
 def on_message(client, userdata, msg):
 	#outfile = open('./output.jpg', 'wb')
-	outfile = open(saveFile, 'wb')
+	outfile = open(args.output, 'wb')
 	outfile.write(msg.payload)
 	outfile.close
 	print('receive topic is {}'.format(msg.topic))
-	print('file {} ({} bytes) write successfully'.format(saveFile, len(msg.payload)))
+	print('file {} ({} bytes) write successfully'.format(args.output, len(msg.payload)))
 
 if __name__=='__main__':
-	_host = socket.gethostbyname(host)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--host', help='mqtt broker', default='esp32-broker.local')
+	parser.add_argument('--port', type=int, help='mqtt port', default=1883)
+	parser.add_argument('--topic', help='mqtt topic', default='/image/esp32cam')
+	parser.add_argument('--output', help='output file name', default='./output.jpg')
+	args = parser.parse_args()
+	print("args.host={}".format(args.host))
+	print("args.port={}".format(args.port))
+	print("args.topic={}".format(args.topic))
+	print("args.output={}".format(args.output))
+
+	_host = socket.gethostbyname(args.host)
 	print("_host={}".format(_host))
 	client_id = f'python-mqtt-{random.randint(0, 1000)}'
-	client = mqtt.Client(client_id=client_id, protocol=mqtt.MQTTv311)
+	client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, client_id)
 	client.on_connect = on_connect
 	client.on_message = on_message
-	client.connect(_host, port=port, keepalive=60)
+	client.connect(_host, port=args.port, keepalive=60)
 	client.loop_forever()
 
